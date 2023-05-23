2868399a2cf82e6bcc2294d230abb9cf40b95227
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-05-23 11:21:44 +0500

    Initial Commit

---
+++ Roll Dice SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Roll Dice SwiftUI
+//
+//  Created by Rasaal Ahmad on 23/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Roll Dice SwiftUI/Roll_Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_Dice_SwiftUIApp.swift
+//  Roll Dice SwiftUI
+//
+//  Created by Rasaal Ahmad on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Roll_Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
