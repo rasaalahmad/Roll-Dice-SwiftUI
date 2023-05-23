aa950abaa108a852c55c753f095a1811303b211a
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-05-23 12:59:54 +0500

    task done

--- Roll Dice SwiftUI/ContentView.swift
+++ Roll Dice SwiftUI/ContentView.swift
@@ -8,19 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Image("GreenBackground")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("DiceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(diceNumber: leftDiceNumber)
+                    DiceView(diceNumber: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button("Roll") {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }
+                .font(.system(size: 50))
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .padding(.horizontal)
+                
+            }
+            
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    var diceNumber:Int
+    var body: some View {
+        Image("Dice\(diceNumber)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
